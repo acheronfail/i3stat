fae1ed2797f17f43bdf517d77c8244441940c602
acheronfail <acheronfail@gmail.com>
2023-05-08 15:39:20 +0930

    qol improvements for the dev repl

--- scripts/run-with-click.sh
+++
@@ -1,9 +0,0 @@
-instance="$1"
-count="${2:-1}"
-
-sleep 1
-echo '['
-for n in `seq 1 1 $count`; do
-  echo '{"name":null,"instance":"'$instance'","button":'$n',"modifiers":[],"x":11,"y":12,"relative_x":15,"relative_y":16,"output_x":9,"output_y":8,"width":13,"height":14}'
-done
-sleep 999
--- scripts/run/index.ts
+++ scripts/run/index.ts
@@ -7,13 +7,6 @@ import stripAnsi from 'strip-ansi';
 
 process.chdir('../..');
 
-function displayHelp() {
-  process.stderr.write(chalk.grey.italic`Usage:
-  click <instance> <button>       e.g.: "click 0 3"
-  repeat                          repeats last command
-`);
-}
-
 // spawn, pipe stdout/err and write initial stdin
 const child = cp.spawn('./target/debug/staturs');
 child.stderr.pipe(process.stderr);
@@ -30,18 +23,63 @@ child.stdout.on('data', (input) => {
     }
 
     const pad = ' '.repeat(process.stdout.columns - stripAnsi(line).length);
-    process.stdout.write('\r' + pad + line);
+    process.stdout.cursorTo(0, process.stdout.rows - 1);
+    process.stdout.clearLine(0);
+    process.stdout.write(pad + line);
     if (i < lines.length - 1) {
       process.stdout.write('\n');
     }
   }
+
+  drawInput();
 });
 
 // listen for commands on stdin, and send JSON to child
-// TODO: have this on a separate line, so we can see what we're typing (like a repl prompt)
 process.stdin.setEncoding('utf8');
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+const CTRL_C = '\u0003';
+const BACKSP = '\u007f';
+const RETURN = '\r';
+let _input = '';
+process.stdin.on('data', (char: string) => {
+  // NOTE: console.log(Buffer.from(char));
+  if (char === CTRL_C) {
+    process.exit(1);
+  } else if (char === BACKSP) {
+    _input = _input.slice(0, -1);
+  } else if (char == RETURN) {
+    handleInput(_input);
+    _input = '';
+  } else if (char.startsWith('\x1B[')) {
+    // just ignore all escape characters for now...
+    // should use this to implement a cursor...
+    _input = '';
+  } else {
+    _input += char;
+  }
+
+  drawInput();
+});
+
+function drawInput() {
+  process.stdout.cursorTo(0, process.stdout.rows);
+  process.stdout.write(`> ${_input}`);
+}
+
+function displayHelp() {
+  // TODO: toggle between short text and full text
+  process.stderr.write(chalk.grey.italic`
+Usage:
+  click <instance> <button>       e.g.: "click 0 3"
+  repeat                          repeats last command
+  quit                            exits
+`);
+}
+
 let last_cmd: string | null = null;
-process.stdin.on('data', (input: string) => {
+function handleInput(input: string) {
   if (input.startsWith('?') || input.startsWith('h')) return displayHelp();
 
   if (input.startsWith('r')) {
@@ -52,33 +90,40 @@ process.stdin.on('data', (input: string) => {
     input = last_cmd;
   }
 
-  const match = /c(?:lick)?\s+(\d)\s+(\d)\s+(s)?/.exec(input);
-  if (!match) return displayHelp();
+  if (input.startsWith('q')) {
+    process.exit();
+  }
 
-  const [, block, btn, shift] = match;
-  const click = {
-    name: null,
-    instance: block,
-    button: parseInt(btn),
-    modifiers: shift ? ['Shift'] : [],
-    x: 11,
-    y: 12,
-    relative_x: 15,
-    relative_y: 16,
-    output_x: 9,
-    output_y: 8,
-    width: 13,
-    height: 14,
-  };
+  if (input.startsWith('c')) {
+    const match = /c(?:lick)?\s+(\d)\s+(\d)(?:\s+(s))?/.exec(input);
+    if (!match) return displayHelp();
 
-  child.stdin.write(JSON.stringify(click) + '\n');
+    const [, block, btn, shift] = match;
+    const click = {
+      name: null,
+      instance: block,
+      button: parseInt(btn),
+      modifiers: shift ? ['Shift'] : [],
+      x: 11,
+      y: 12,
+      relative_x: 15,
+      relative_y: 16,
+      output_x: 9,
+      output_y: 8,
+      width: 13,
+      height: 14,
+    };
+
+    child.stdin.write(JSON.stringify(click) + '\n');
+  }
+
   last_cmd = input;
-});
+}
 
 function formatLine(line: string) {
-  const items: any[] = JSON.parse(line.slice(0, -1));
+  const items: Record<string, any>[] = JSON.parse(line.slice(0, -1));
 
-  let result: any[] = [];
+  let result: string[] = [];
   for (const item of items) {
     if (!item.full_text) {
       continue;
