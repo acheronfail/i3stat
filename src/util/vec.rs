fae1123a8fd8ba28b1706b54f2625fa3e06ae086
acheronfail <acheronfail@gmail.com>
2023-06-20 12:14:13 +0930

    no need for hashset

--- src/util/vec.rs
+++ src/util/vec.rs
@@ -1,26 +1,37 @@
-use std::collections::HashSet;
-
 /// Sort a vec in place, by providing a list of desired indices.
 /// Thanks to: https://stackoverflow.com/a/69774341/5552584
 pub fn sort_by_indices<T>(slice: &mut [T], mut indices: Vec<usize>) {
     // assert `indices` is valid input
     {
-        let mut seen = HashSet::new();
         let len = slice.len();
+        assert_eq!(
+            len,
+            indices.len(),
+            "indices must have the same length as input slice"
+        );
+
+        // check all indices are in bounds
         for idx in &indices {
-            assert!(*idx < len, "indices contains out of bounds index: {}", idx);
+            assert!(*idx < len, "indices contains out of bounds index: {}", *idx);
+        }
+
+        // check no duplicates
+        for i in 0..len {
+            let idx = indices[i];
+            let idx = if idx >= len { idx - len } else { idx };
             assert!(
-                seen.insert(idx),
+                indices[idx] < len,
                 "indices contained duplicate index: {}",
                 idx
             );
+
+            indices[idx] += len;
         }
 
-        assert_eq!(
-            len,
-            seen.len(),
-            "indices must have the same length as input slice"
-        );
+        // undo our changes to `indices` before we use it in the sort
+        for idx in &mut indices {
+            *idx -= len;
+        }
     }
 
     // perform sort
@@ -54,6 +65,22 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "indices contains out of bounds index: 4")]
+    fn oob_indices2() {
+        let indices = vec![0, 4, 2, 1];
+        let mut data = vec!["a", "b", "c", "d"];
+        sort_by_indices(&mut data, indices);
+    }
+
+    #[test]
+    #[should_panic(expected = "indices contains out of bounds index: 4")]
+    fn oob_indices3() {
+        let indices = vec![0, 1, 4];
+        let mut data = vec!["a", "b", "c"];
+        sort_by_indices(&mut data, indices);
+    }
+
+    #[test]
     #[should_panic(expected = "indices contained duplicate index: 1")]
     fn duplicate_indices() {
         let indices = vec![1, 2, 3, 1];
@@ -70,9 +97,9 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "indices contains out of bounds index: 4")]
+    #[should_panic(expected = "indices must have the same length as input slice")]
     fn longer_indices() {
-        let indices = vec![3, 2, 1, 0, 4];
+        let indices = vec![3, 2, 1, 0, 0];
         let mut data = vec!["a", "b", "c", "d"];
         sort_by_indices(&mut data, indices);
     }
