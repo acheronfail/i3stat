fae1ca33121470d3326b5ea9c7759c00519b419c
acheronfail <acheronfail@gmail.com>
2023-07-11 22:11:32 +0930

    fix debug representation of MacAddr

--- src/util/netlink/mod.rs
+++ src/util/netlink/mod.rs
@@ -20,7 +20,7 @@ impl Debug for MacAddr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!(
             "MacAddr({})",
-            self.octets.map(|o| format!("{:2x}", o)).join(":")
+            self.octets.map(|o| format!("{:02x}", o)).join(":")
         ))
     }
 }
@@ -80,4 +80,15 @@ pub struct NetlinkInterface {
     pub name: Arc<str>,
     pub mac_address: Option<MacAddr>,
     pub ip_addresses: IndexSet<IpAddr>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn debug() {
+        let mac = MacAddr::from(&[1, 42, 83, 124, 165, 206]);
+        assert_eq!(format!("{:?}", mac), "MacAddr(01:2a:53:7c:a5:ce)");
+    }
 }
