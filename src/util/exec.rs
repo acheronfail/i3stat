fae1e994e4c47e8a762d50ca1f2bab44fb2eede8
acheronfail <acheronfail@gmail.com>
2023-10-15 07:14:42 +1030

    update exec to not block the main thread

--- src/util/exec.rs
+++ src/util/exec.rs
@@ -1,19 +1,25 @@
-use tokio::process::Command;
+use std::process::Command;
+use std::thread;
 
 /// Used when bar items need to run an external command. It won't block, and also
 /// won't return any error: it shouldn't crash the app if the child process fails
 /// in any way (just like i3 handles commands).
+///
+/// Unfortunately there's no `Command::try_wait` equivalent on `tokio::process::Command`,
+/// so this spawns a separate thread for each command, in case the command blocks or waits.
 pub async fn exec(cmd: impl AsRef<str>) {
-    let cmd = cmd.as_ref();
-    log::debug!("exec: command --> {} <--", cmd);
+    let cmd = cmd.as_ref().to_owned();
+    log::debug!("exec: command --> {} <--", &cmd);
 
-    let child = Command::new("sh").arg("-c").arg(cmd).output();
-    match child.await {
-        Ok(output) => {
-            if !output.status.success() {
-                log::warn!("exit: command --> {} <-- {}", cmd, output.status);
+    thread::spawn(move || {
+        let child = Command::new("sh").arg("-c").arg(&cmd).output();
+        match child {
+            Ok(output) => {
+                if !output.status.success() {
+                    log::warn!("exit: command --> {} <-- {}", cmd, output.status);
+                }
             }
+            Err(e) => log::error!("fail: command --> {} <-- {}", cmd, e),
         }
-        Err(e) => log::error!("fail: command --> {} <-- {}", cmd, e),
-    }
+    });
 }
